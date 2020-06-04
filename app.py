bca1452e49568bf270ecbdd1af441909edd8ded4
ViankaG <ZHAC234@live.rhul.ac.uk>
2020-06-04 01:06:34 +0100

    handle the POST request for secondary colour of flag

--- app.py
+++ app.py
@@ -29,12 +29,13 @@ def create_buggy():
     try:
       qty_wheels = request.form['qty_wheels']
       flag_color = request.form['flag_color']
-      msg = f"qty_wheels={qty_wheels}, flag_color={flag_color}"
+      flag_color_secondary = request.form['flag_color_secondary']
+      msg = f"qty_wheels={qty_wheels}, flag_color={flag_color}, flag_color_secondary={flag_color_secondary}"
       with sql.connect(DATABASE_FILE) as con:
         cur = con.cursor()
         cur.execute(
-           "UPDATE buggies set qty_wheels=?,  flag_color=? WHERE id=?",
-           (qty_wheels, flag_color, DEFAULT_BUGGY_ID)
+           "UPDATE buggies set qty_wheels=?,  flag_color=?, flag_color_secondary=? WHERE id=?",
+           (qty_wheels, flag_color, flag_color_secondary, DEFAULT_BUGGY_ID)
         )
         con.commit()
         msg = "Record successfully saved"
